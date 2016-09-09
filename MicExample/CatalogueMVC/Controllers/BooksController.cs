c3caa0f9886de62ce22c9d98acb0b628ce27cba4
Hovo <hovo@live.eu>
2016-09-09 14:49:38 +0400

    Working

---
+++ MicExample/CatalogueMVC/Controllers/BooksController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookEntitiesDAL;
+using System.ComponentModel.DataAnnotations;
+
+namespace CatalogueMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        private BookDBEntities db = new BookDBEntities();
+
+        // GET: Books
+        public async Task<ActionResult> Index()
+        {
+            var books = db.Books.Include(b => b.Author).Include(b => b.Country).Include(b => b.Image);
+            return View(await books.ToListAsync());
+        }
+
+        // GET: Books/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = await db.Books.FindAsync(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // GET: Books/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            ViewBag.AuthorID = new SelectList(db.Authors, "AuthorID", "FullName");
+            ViewBag.CountryID = new SelectList(db.Countries, "CountryID", "Name");
+            ViewBag.ImageID = new SelectList(db.Images, "ImageID", "ImageID");
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "BookID,Title,AuthorID,CountryID,Price,Description,PagesCount,ImageID")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Books.Add(book);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AuthorID = new SelectList(db.Authors, "AuthorID", "FullName", book.AuthorID);
+            ViewBag.CountryID = new SelectList(db.Countries, "CountryID", "Name", book.CountryID);
+            ViewBag.ImageID = new SelectList(db.Images, "ImageID", "ImageID", book.ImageID);
+            return View(book);
+        }
+
+        // GET: Books/Edit/5
+        [Authorize]
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = await db.Books.FindAsync(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AuthorID = new SelectList(db.Authors, "AuthorID", "FullName", book.AuthorID);
+            ViewBag.CountryID = new SelectList(db.Countries, "CountryID", "Name", book.CountryID);
+            ViewBag.ImageID = new SelectList(db.Images, "ImageID", "ImageID", book.ImageID);
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "BookID,Title,AuthorID,CountryID,Price,Description,PagesCount,ImageID")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(book).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AuthorID = new SelectList(db.Authors, "AuthorID", "FullName", book.AuthorID);
+            ViewBag.CountryID = new SelectList(db.Countries, "CountryID", "Name", book.CountryID);
+            ViewBag.ImageID = new SelectList(db.Images, "ImageID", "ImageID", book.ImageID);
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        [Authorize]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = await db.Books.FindAsync(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Book book = await db.Books.FindAsync(id);
+            db.Books.Remove(book);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MicExample/CatalogueMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CatalogueMVC.Startup))]
+namespace CatalogueMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
